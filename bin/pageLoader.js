e07ab6226582fcc80f3809865d0755fb490ce581
Roman <deepspiral@yandex.ru>
2020-12-18 08:07:42 +0300

    init commander

--- bin/pageLoader.js
+++ bin/pageLoader.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-import commander from 'commander';
+import { program } from 'commander';
+import load from '../src';
+
+program
+  .option('-o, --output [value]', 'output destination path', process.cwd())
+  .arguments('<url>')
+  .action((url) => load(url, program.output))
+  .parse(process.argv);
