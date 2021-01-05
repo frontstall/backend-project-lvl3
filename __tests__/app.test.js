cbb813ccabee02bf7348af299dab4d2b5a96dc62
Roman Makarov <deepspiral@yandex.ru>
2021-01-05 17:18:45 +0300

    add images download

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,11 +5,12 @@ import { promises as fs } from 'fs';
 import nock from 'nock';
 import axios from 'axios';
 import adapter from 'axios/lib/adapters/http';
+import _ from 'lodash';
 
 import load from '../src';
 
-const hostname = 'https://ru.hexlet.io';
-const pathname = '/professions';
+const hostname = 'https://smartrw.github.io';
+const pathname = '/nerds';
 const { href } = new URL(pathname, hostname);
 const getPathToFixture = (filename) => path.resolve('__fixtures__', filename);
 
@@ -29,14 +30,24 @@ beforeEach(async () => {
 });
 
 test('application', async () => {
-  const fixtureFilename = 'professions.html';
-  const pathToFixture = getPathToFixture(fixtureFilename);
-  const fixture = await fs.readFile(pathToFixture, 'utf-8');
+  const expectedFilename = 'smartrw-github-io-nerds.html';
+  const expectedAssetsDirectoryName = 'smartrw-github-io-nerds_files';
+  const expectedPathToAssetsDirectory = path.resolve(pathToTempDir, expectedAssetsDirectoryName);
+  const index = await fs.readFile(getPathToFixture('index.html'), 'utf-8');
+  const expected = await fs.readFile(getPathToFixture(expectedFilename), 'utf-8');
   nock(hostname)
     .get(pathname)
-    .reply(200, fixture);
+    .reply(200, index);
 
   const pathToLoadedFile = await load(href, pathToTempDir);
-  expect(pathToLoadedFile).toBe(path.resolve(pathToTempDir, 'ru-hexlet-io-professions.html'));
-  expect(await fs.readFile(pathToLoadedFile, 'utf-8')).toBe(fixture);
+  expect(await fs.readFile(pathToLoadedFile, 'utf-8')).toBe(expected);
+
+  expect(pathToLoadedFile).toBe(path.resolve(pathToTempDir, expectedFilename));
+
+  const { path: pathToAssetsDirectory } = await fs.opendir(expectedPathToAssetsDirectory);
+  expect(pathToAssetsDirectory).toBe(expectedPathToAssetsDirectory);
+
+  const assetsFilenames = await fs.readdir(pathToAssetsDirectory);
+  const expectedAssetsFilenames = await fs.readdir(expectedPathToAssetsDirectory);
+  expect(_.difference(assetsFilenames, expectedAssetsFilenames)).toEqual([]);
 });
--- src/index.js
+++ src/index.js
@@ -1,26 +1,84 @@
-import axios from 'axios';
-import { promises as fs } from 'fs';
-import path from 'path';
+// @ts-check
 
-const urlToFilename = (string) => {
-  const placeholder = '-';
-  const extension = '.html';
+import axios from 'axios';
+import { promises as fs, createWriteStream } from 'fs';
+import path from 'path';
+import cheerio from 'cheerio';
+
+const imgExtensions = ['.png', '.jpg', '.jpeg'];
+
+const stringify = (string, placeholder = '-') => string.replace(/[^A-Za-z0-9]/g, placeholder);
+
+const stringifyUrl = (string) => {
   const url = new URL(string);
-  const urlWithoutProtocol = `${url.hostname}${url.pathname}`;
-  const filename = urlWithoutProtocol
-    .replace(/[^A-z]/g, placeholder)
-    .concat(extension);
-  return filename;
+  return stringify(`${url.hostname}${url.pathname}`);
 };
 
+const downloadImage = (url, pathToSave) => new Promise((resolve, reject) => {
+  let stream;
+  axios
+    .get(url, { responseType: 'stream' })
+    .then(({ data }) => {
+      stream = createWriteStream(pathToSave);
+      stream.on('finish', resolve);
+      stream.on('error', reject);
+      data.pipe(stream);
+    })
+    .catch((e) => {
+      if (stream) stream.destroy();
+      reject(e);
+    });
+});
+
 const load = (url, outputDirPath) => {
-  let pathToFile;
+  const fileName = stringifyUrl(url);
+  const pathToFile = path.format({
+    dir: outputDirPath,
+    name: fileName,
+    ext: '.html',
+  });
+  const dirname = fileName.concat('_files');
+  const pathToResources = path.join(outputDirPath, dirname);
   return axios.get(url)
     .then((response) => {
       const { data } = response;
-      const fileName = urlToFilename(url);
-      pathToFile = path.join(outputDirPath, fileName);
-      return fs.writeFile(pathToFile, data, 'utf-8');
+      const $ = cheerio.load(data);
+      const imgs = $('img').filter((_i, el) => {
+        const { ext } = path.parse(el.attribs.src);
+
+        return imgExtensions.includes(ext);
+      });
+
+      const imagesData = [];
+      imgs.each((_i, el) => {
+        const { dir, name, ext } = path.parse(el.attribs.src);
+        const pathToImage = path.format({ dir, name });
+        const imgFilename = [stringifyUrl(url), stringify(pathToImage)].join('-');
+        const newSrc = path.format({
+          dir: dirname,
+          name: imgFilename,
+          ext,
+        });
+
+        const { href } = new URL(el.attribs.src, url);
+
+        imagesData.push({
+          href,
+          filename: path.format({ name: imgFilename, ext }),
+        });
+        el.attribs.src = newSrc;
+      });
+
+      const html = $.html();
+
+      return Promise.all([imagesData, fs.writeFile(pathToFile, html, 'utf-8'), fs.mkdir(pathToResources), fs.writeFile('/home/roman/projects/backend-project-lvl3/__fixtures__/smartrw-github-io-nerds.html', html)]);
+    })
+    .then(([imagesData]) => {
+      const promises = imagesData.map(({ href, filename }) => {
+        const pathToImg = path.join(pathToResources, filename);
+        return downloadImage(href, pathToImg);
+      });
+      return Promise.allSettled(promises);
     })
     .then(() => pathToFile)
     .catch((err) => {
