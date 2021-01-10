6e71b25a9e843137bbad091e9e49ca6fe496fc39
Roman Makarov <deepspiral@yandex.ru>
2021-01-10 16:00:08 +0300

    add test for images loading

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,6 +13,53 @@ const pathname = '/nerds';
 const { href } = new URL(pathname, hostname);
 const getPathToFixture = (filename) => path.resolve('__fixtures__', filename);
 
+const assetsConfig = {
+  'logo-1.png': {
+    type: 'binary',
+    dir: 'img',
+  },
+  'logo-2.png': {
+    type: 'binary',
+    dir: 'img',
+  },
+  'logo-3.png': {
+    type: 'binary',
+    dir: 'img',
+  },
+  'logo-4.png': {
+    type: 'binary',
+    dir: 'img',
+  },
+  'nerds-illustration.png': {
+    type: 'binary',
+    dir: 'img',
+  },
+  'normalize-min.css': {
+    type: 'text',
+    dir: 'css',
+  },
+  'style-min.css': {
+    type: 'text',
+    dir: 'css',
+  },
+  'script-min.js': {
+    type: 'text',
+    dir: 'js',
+  },
+};
+
+const getHandler = (assetType, pathToAsset) => {
+  const config = {
+    binary: () => createReadStream(pathToAsset),
+    text: async () => {
+      const asset = await fs.readFile(pathToAsset);
+      return asset;
+    },
+  };
+
+  return config[assetType];
+};
+
 let pathToTempDir;
 
 beforeAll(() => {
@@ -37,21 +84,15 @@ test('application', async () => {
     .get(pathname)
     .reply(200, fixture);
 
-  [
-    'logo-1.png',
-    'logo-2.png',
-    'logo-3.png',
-    'logo-4.png',
-    'nerds-illustration.png',
-  ].forEach((imgName) => {
-    const src = path.join(pathname, 'img', imgName);
+  Object.entries(assetsConfig).forEach(([assetName, { dir, type }]) => {
+    const route = path.join(pathname, dir, assetName);
+    const pathToAsset = getPathToFixture(path.join('assets', dir, assetName));
+
+    const handler = getHandler(type, pathToAsset);
 
     nock(hostname)
-      .get(src)
-      .reply(200, () => {
-        const pathToImg = getPathToFixture(path.join('assets', imgName));
-        return createReadStream(pathToImg);
-      });
+      .get(route)
+      .reply(200, handler);
   });
 
   const pathToLoadedFile = await load(href, pathToTempDir);
